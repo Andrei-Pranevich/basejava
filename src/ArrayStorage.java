c8c40f78980bfb7d46e1c6a87ffd856317c56674
Andrei-Pranevich <pranevich@grsu.by>
2019-01-14 21:46:22 +0300

    Changes for ArrayStorage.java

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -3,28 +3,59 @@
  */
 public class ArrayStorage {
     Resume[] storage = new Resume[10000];
+    private int nElem = 0; // количество элементов в массиве
 
     void clear() {
+        for (int i = 0; i < nElem; i++) {
+            storage[i] = null;
+        }
+        nElem=0;
     }
 
     void save(Resume r) {
+        if (r == null){
+            return;
+        }
+
+        storage[nElem] = r;
+        nElem++;
     }
 
     Resume get(String uuid) {
+        for (int i = 0; i < nElem; i++) {
+            if (storage[i].uuid == uuid) {
+                return storage[i];
+            }
+        }
         return null;
     }
 
     void delete(String uuid) {
+        for (int i = 0; i < nElem; i++) {
+            if (storage[i].uuid == uuid) {
+                for (int j = i; j < nElem; j++) {
+                    storage[j] = storage[j + 1];
+                }
+                nElem--;
+            }
+        }
+
     }
 
     /**
      * @return array, contains only Resumes in storage (without null)
      */
     Resume[] getAll() {
-        return new Resume[0];
+        Resume[] mas = new Resume[nElem];
+        for (int i = 0; i < nElem; i++) {
+            mas[i] = storage[i];
+        }
+        return mas;
+
     }
 
     int size() {
-        return 0;
+        return nElem;
     }
+
 }
